0920f5da25a87eb2ae0ed9e220b446cb48b80ee0
Abderrahmane Maftah <78481502+mafghal@users.noreply.github.com>
2022-11-25 15:30:41 +0000

    t

---
+++ Brief7/index.js
@@ -0,0 +1,9 @@
+let article = document.getElementById("Article").value;
+let marque = document.getElementById("Marque").value;
+let prix = document.getElementById("prix").value;
+let date = document.getElementById("date").value;
+
+function redirect(){
+    var x = 0;
+    if (x >)
+}
