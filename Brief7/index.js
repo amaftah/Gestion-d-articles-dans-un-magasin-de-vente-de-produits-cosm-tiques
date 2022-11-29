edde05c584c683f873c78668fd9b5f26ea184d56
Abderrahmane Maftah <78481502+mafghal@users.noreply.github.com>
2022-11-29 11:23:58 +0000

    t

--- Brief7/index.js
+++ Brief7/index.js
@@ -1,92 +1,135 @@
-function reflechire() {
-    let ajouter = document.querySelector("#ajouter");
-    ajouter.disabled = true;
-}
-
-function Ajouter() {
-    let Article = document.getElementById('Article').value;
-    let Marque = document.getElementById('Marque').value;
-    let Prix = document.getElementById('prix').value;
-    let Date = document.getElementById('date').value;
-    let Type = document.getElementById('TYpe').value;
-    let oui = document.getElementById('oui');
-    let non = document.getElementById('non');
-
-
-    let table = document.querySelector('table');
-    const tBody = document.querySelector('tbody');
-
-    for (let i = 0; i < 1; i++) {
-        const row = document.createElement("tr")
-            for (let j = 0; j < 7; j++) {
-                const cell = document.createElement("td");
-                const suppButton = document.createElement("button");
-                const modiButton = document.createElement("button");
-                const nomOutput = document.querySelector("#nomOutput");
-
+// var rIndex,
+//     table = document.getElementById("table");
+let table = document.getElementById('table')
+function checkEmptyInput(){
+            var isEmpty = false,
+                nom = document.getElementById("Article").value,
+                marque = document.getElementById("Marque").value,
+                prix = document.getElementById("prix").value;
+                date = document.getElementById("date").value;
+                type = document.getElementById("TYpe").value;
+                promotion = document.getElementById("promotion").value;
                 
-                row.appendChild(cell);
-                if (j == 0 && Article.match(/[0-9]/g)) {
-                    nomOutput.innerText = "Erreur";
-                    nomOutput.style.color = "red";
-                    
-                }
-                else if ( j == 0 && Article.length > 30){
-                    Ajouter.disabled = false;
-                    cell.innerText = Nom;
-                }
-                if (j == 0 && Nom.match(/[0-9]/g)) {
-                    nomOutput.textContent = "Erreur";
-                    nomOutput.style.color = "red";
-                    
-                }
-                else if ( j == 0 && Nom.length > 30){
-                    ajouter.disabled = false;
-                    cell.textContent = Article;
-                }
-                    if(j == 1){
-                     cell.textContent = Marque;
-                 }
-                 if(j == 2){
-                     cell.textContent = Prix;
-                 }
-                 if(j == 3){
-                     cell.textContent = Type;
-                 }
-                 if(j == 4){ 
-                     cell.textContent = Date;
-                 }
-                 if(j == 5){
-                     cell.textContent = Nom;
-                 }
-                 if(j == 6){
-                     // iserer un botton de modification  
-                        modiButton.innerText = "Modifier";
-                     modiButton.setAttribute("class", "modifier");
-                     cell.appendChild(modiButton);
-                     // iserer un botton de suppression 
-                     suppButton.innerText = "Supprimer";
-                     suppButton.setAttribute("class", "supprimer");
-                     suppButton.setAttribute("onclick", "deletRow(this)");
-                     cell.appendChild(suppButton);
-                 }
+        
+            if(nom === ""){
+                alert("il faux écrire un nom");
+                isEmpty = true;
             }
-            table.appendChild(row);
-
- 
-
-
-
-
-
-    }
+            else if(marque === ""){
+                alert("il faux écrire la marque");
+                isEmpty = true;
+            }
+            else if(prix === ""){
+                alert("il faux ajouter un prix");
+                isEmpty = true;
+            }
+            else if(date === ""){
+              alert("il faux ajouter une date");
+              isEmpty = true;
+            }
+            else if(type === ""){
+              alert("il faux ajouter un type");
+              isEmpty = true;
+            }
+            else if(promotion === ""){
+              alert("il faux ajouter un promotion");
+              isEmpty = true;
+            }
+            return isEmpty;
 }
-function deletRow(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("myTable").deleteRow(i);
 
-const btn = document.querySelector('.supprimer');
-     btn.addEventListener("click", function()
-     { btn.closest("tr").remove(); });
-    
+let btn =document.getElementById("ajouter");
+btn.addEventListener("click", insertNewRow);
+function insertNewRow(e){
+
+  e.preventDefault();
+
+  if (!checkEmptyInput()){ }
+  // creat line 
+var line = document.createElement("tr");
+line.classList.add('newLine')
+table.append(line)
+// add  td
+var nom = document.getElementById("Article").value;
+var marque = document.getElementById("Marque").value;
+var prix = document.getElementById("prix").value
+var date = document.getElementById("date").value;
+var type = document.getElementById("TYpe").value;
+
+var oui = document.getElementById("oui")
+var non = document.getElementById("non")
+
+var  Narticle = document.createElement("td");
+line.append(Narticle)
+var  NMarque = document.createElement("td");
+line.append(NMarque)
+var  Nprix = document.createElement("td");
+line.append(Nprix)
+var  Ndate = document.createElement("td");
+line.append(Ndate)
+var  Ntype = document.createElement("td");
+line.append(Ntype)
+var  Npromotion = document.createElement("td");
+line.append(Npromotion)
+
+Narticle.innerHTML = nom ;
+NMarque.innerHTML = marque;
+Nprix.innerHTML = prix;
+Ndate.innerHTML = date;
+Ntype.innerHTML = type;
+if (oui.checked  ) {
+  console.log("oui")
+  Npromotion.innerHTML = oui.value;
+}
+if (non.checked  ) {
+  console.log("non")
+  Npromotion.innerHTML = non.value;  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var newRow = table.insertRow(table.length),
+  //     cell2 = newRow.insertCell(1),
+  //     cell3 = newRow.insertCell(2),
+  //     cell4 = newRow.insertCell(3),
+  //     cell5 = newRow.insertCell(4),
+  //     cell6 = newRow.insertCell(5),
+  //     cell7 = newRow.insertCell(6),
+
+  //   article = document.getElementById("Article").value,
+  //   marque = document.getElementById("Marque").value,
+  //   prix = document.getElementById("prix").value,
+  //   date = document.getElementById("date").value,
+  //   type = document.getElementById("TYpe").value,
+  //   promotion = document.getElementById("promotion");
+  //    cell1.innerHTML = article;
+  //    cell2.innerHTML = marque;
+  //    cell3.innerHTML = prix
+  //    cell4.innerHTML = date
+  //    cell5.innerHTML = type
+  //    cell6.innerHTML = promotion
+    //  selectedRowToInput();
+   
+ 
 }
